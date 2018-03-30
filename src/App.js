7eeee8d42d07bffd420e0bd96145e3a823d80fd7
Igor Cuckovic <cuckovic@gmail.com>
2018-03-30 14:46:39 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from "react";
 import ListContacts from "./ListContacts";
-
+import * as ContactsAPI from "./utils/ContactsAPI";
 
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      },
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      }
+    contacts: []
+  }
 
-    ]
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState(() => {
+        return {
+          contacts
+        }
+      })
+    })
   }
 
   removeContact = (contact) => {
@@ -38,7 +28,7 @@ class App extends Component {
       }
     });
 
-    //console.log(contact.id)
+    ContactsAPI.remove(contact)
   }
 
   render() {
