43f1fea3cf3691e9e9208e1eaf7a75302a3d29a2
Igor Cuckovic <cuckovic@gmail.com>
2018-03-30 11:32:34 +0200

    contacts

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
+
+class App extends Component {
+
+  state = {
+    contacts: [
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      },
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      }
+
+    ]
+  }
+
+  render() {
+    const {contacts} = this.state;
+    return (
+      <div>
+        <ListContacts contacts={contacts} />
+      </div>
+    )
+  }
+};
+
+export default App;
