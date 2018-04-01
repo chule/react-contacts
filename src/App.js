b1d7006cc0e090b2621f60adbee0b01d4f9cc751
Igor Cuckovic <cuckovic@gmail.com>
2018-04-02 00:11:18 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -33,6 +33,15 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: [...currentState.contacts, contact]
+        }))
+      })
+  }
+
   render() {
     const { contacts } = this.state;
     return (
@@ -45,7 +54,14 @@ class App extends Component {
           />)
         } />
 
-        <Route path="/create" component={CreateContact}
+        <Route path="/create" render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}
         />
 
 
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react"
 import { Link } from 'react-router-dom'
-import ImageInput from "./ImageInput";
+import ImageInput from "./ImageInput"
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const values = serializeForm(e.target, { hash: true })
+        //console.log('values', values)
+
+        if (this.props.onCreateContact) {
+            this.props.onCreateContact(values)
+        }
+
+    }
 
     render() {
         return (
@@ -13,18 +25,19 @@ class CreateContact extends Component {
                 >
                     Close
                 </Link>
-                <form className="create-contact-form">
-                    <ImageInput 
+                <form onSubmit={this.handleSubmit} className="create-contact-form">
+                    <ImageInput
                         className="create-contact-avatar-input"
                         name="avatarURL"
                         maxheight={64}
                     />
+
+                    <div className="create-contact-details">
+                        <input type="text" name="name" placeholder="Name" />
+                        <input type="text" name="handle" placeholder="Handle " />
+                        <button>Add Contact</button>
+                    </div>
                 </form>
-                <div className="create-contact-details">
-                    <input type="text" name="name" placeholder="Name" />
-                    <input type="text" name="handle" placeholder="Handle "/>
-                    <button>Add Contact</button>
-                </div>
             </div>
         )
     }
